4a528a173b9389eaaa6e565111da8ba3c54c51bd
Jose Luis Martin Blanco <jmb0049@alu.medac.es>
2026-05-11 09:26:48 +0200

    Improvement made to my branch

--- Conversor.java
+++ Conversor.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Conversor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("--- CONVERSOR DE TEMPERATURA (Celsius a Fahrenheit) ---");
+        System.out.println("--- CONVERSOR ACTUALIZADO POR JOSE LUIS ---");
         System.out.print("Ingresa grados Celsius: ");
-        
         double celsius = sc.nextDouble();
         double fahrenheit = (celsius * 9/5) + 32;
-        
-        System.out.println(celsius + " grados Celsius son " + fahrenheit + " grados Fahrenheit.");
+        double kelvin = celsius + 273.15; 
+        System.out.println("Fahrenheit: " + fahrenheit + " | Kelvin: " + kelvin);
         sc.close();
     }
 }
